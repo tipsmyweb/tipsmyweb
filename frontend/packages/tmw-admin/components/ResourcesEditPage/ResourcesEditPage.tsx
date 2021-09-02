8d09913a9a2ba9cfb30f15fbc2ec28f01681ec62
Josselin Pennors <josselin.pennors@hotmail.fr>
2021-09-02 22:41:41 +0200

    Fix

--- frontend/packages/tmw-admin/components/ResourcesEditPage/ResourcesEditPage.tsx
+++ frontend/packages/tmw-admin/components/ResourcesEditPage/ResourcesEditPage.tsx
@@ -16,7 +16,12 @@ import {
 import { ActionMessage } from 'tmw-admin/components/ActionMessage';
 import { FormFooter } from 'tmw-admin/components/FormFooter';
 import { PageHeader } from 'tmw-admin/components/PageHeader';
-import { ADMIN_APP_ROUTES, LOCALES, LOCALES_NAMES } from 'tmw-admin/constants/app-constants';
+import {
+    ADMIN_APP_ROUTES,
+    LOCALES,
+    LOCALES_NAMES,
+    RESOURCES_IMAGE_BASE_URL,
+} from 'tmw-admin/constants/app-constants';
 import { Resource, TagsMap } from 'tmw-admin/constants/app-types';
 import {
     serializePricesFromAPI,
@@ -68,11 +73,17 @@ export const ResourcesEditPage: React.FunctionComponent = () => {
             .then(res => {
                 const resource = serializeResourceFromAPI(res.data);
                 setResource(resource);
+                updateImageFromResource(resource);
             })
             .catch(() => {
                 setErrorMessage('Error while fetching resource from the API.');
                 setCanEdit(false);
             });
+    };
+
+    const updateImageFromResource = (resource: Resource): void => {
+        if (resource.iconFilename)
+            setResourceImageTempURL(`${RESOURCES_IMAGE_BASE_URL}${resource.iconFilename}`);
     };
 
     const fetchPricesOptions = async (): Promise<void> => {
